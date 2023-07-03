7ee821cea008c0fcda6502604d1c5f500888cfa5
SeTruphe <albert.lattke@gmail.com>
2023-07-03 11:32:15 +0200

    Combining functions and more WER
    "transcribe" and "transcribe_to_txt" have been merged into one function. Also WER metrics for "medium" whisper model have been added
    
    Added:
     - WER metrics for "medium" whisper model with helsinki eng-ger
    
    Changes:
     - transcribe functions merged

--- radio-analyzer/utils.py
+++ radio-analyzer/utils.py
@@ -31,7 +31,6 @@ def split_audio(path, safe_path, opt_folder_name=None):
     :return: returns the complete folder path to the audio chunks
     """
 
-
     file_format = path.split('.', 1)[1]
     recording = AudioSegment.from_file(path, format=path.split('.', 1)[1])
 
@@ -65,8 +64,7 @@ def split_audio(path, safe_path, opt_folder_name=None):
     return folder_path
 
 
-def transcribe_to_txt(chunk_path, whisper_model="large", german_translation_model="helsinki"):
-
+def transcribe(chunk_path, whisper_model="large", german_translation_model="helsinki", internal_mode=False, to_txt=False):
     """
         :param chunk_path: path to the audio chunks. Chunks cannot be longer than 30 seconds
         :param whisper_model: Size of the whisper model you want to use.
@@ -74,8 +72,11 @@ def transcribe_to_txt(chunk_path, whisper_model="large", german_translation_mode
                 For references, visit: https://github.com/openai/whisper
         :param german_translation_model: model for the translation english-german
                 Available options: helsinki, nllb
+        :param internal_mode: if true, explanatory and delimiting strings are not addet to the transcript/translation
+                lists for further internal usage
+        :param to_txt: if true, the transcript and translations will be saved into txt files in the chunk folder
+        :return: returns the transcribed and translated lists
     """
-
 
     # german-english-model
 
@@ -98,12 +99,13 @@ def transcribe_to_txt(chunk_path, whisper_model="large", german_translation_mode
     text_english = []
     text_german = []
 
-    text_original.append("Model: whisper-large-v2 Task: transcribe\n".encode('utf-8'))
-    text_original.append(("###START OF ORIGINAL TRANSCRIPTION FROM FILE " + str(filename) + "###").encode('utf-8'))
-    text_english.append("Model: whisper-large-v2 Task: translate original-english\n")
-    text_english.append("###START OF ENGLISH TRANSLATION FROM FILE " + str(filename) + "###")
-    text_german.append("Model: Helsinki-nlp Task: translate english-german\n")
-    text_german.append("###START OF GERMAN TRANSLATION FROM FILE " + str(filename) + "###")
+    if not internal_mode:
+        text_original.append(("Model: whisper-" + whisper_model + " Task: transcribe\n").encode('utf-8'))
+        text_original.append(("###START OF ORIGINAL TRANSCRIPTION FROM FILE " + str(filename) + "###").encode('utf-8'))
+        text_english.append("Model: whisper-large-v2 Task: translate original-english\n")
+        text_english.append("###START OF ENGLISH TRANSLATION FROM FILE " + str(filename) + "###")
+        text_german.append("Model: Helsinki-nlp Task: translate english-german\n")
+        text_german.append("###START OF GERMAN TRANSLATION FROM FILE " + str(filename) + "###")
 
     model = whisper.load_model(whisper_model)
 
@@ -115,24 +117,23 @@ def transcribe_to_txt(chunk_path, whisper_model="large", german_translation_mode
 
         # Transcription + English translation
 
-        original_transcript = model.transcribe(os.path.join(chunk_path, file), task="transcribe")["text"].encode('utf-8')
+        original_transcript = model.transcribe(os.path.join(chunk_path, file), task="transcribe")["text"].encode(
+            'utf-8')
         text_english = model.transcribe(os.path.join(chunk_path, file), task="translate")["text"]
 
-        text_original.append(("\n######## START OF " + str(os.path.basename(file)).upper()
-                              + " (" + str(datetime.timedelta(seconds=time_start)) + " - "
-                              + str(datetime.timedelta(seconds=time_end)) + "s) " + "########\n").encode('utf-8'))
+        if not internal_mode:
+            text_original.append(("\n######## START OF " + str(os.path.basename(file)).upper()
+                                  + " (" + str(datetime.timedelta(seconds=time_start)) + " - "
+                                  + str(datetime.timedelta(seconds=time_end)) + "s) " + "########\n").encode('utf-8'))
+            text_english.append("\n######## START OF " + str(os.path.basename(file)).upper()
+                                + " (" + str(datetime.timedelta(seconds=time_start))
+                                + " - " + str(datetime.timedelta(seconds=time_end)) + "s) " + "########\n")
+            text_german.append("\n######## START OF " + str(os.path.basename(file)).upper()
+                               + " (" + str(datetime.timedelta(seconds=time_start)) + " - "
+                               + str(datetime.timedelta(seconds=time_end)) + "s) " + "########\n")
+
         text_original.append(original_transcript.strip())
-
-        text_english.append("\n######## START OF " + str(os.path.basename(file)).upper()
-                            + " (" + str(datetime.timedelta(seconds=time_start))
-                            + " - " + str(datetime.timedelta(seconds=time_end)) + "s) " + "########\n")
         text_english.append(text_english.strip())
-
-        # Helsinki
-
-        text_german.append("\n######## START OF " + str(os.path.basename(file)).upper()
-                           + " (" + str(datetime.timedelta(seconds=time_start)) + " - "
-                           + str(datetime.timedelta(seconds=time_end)) + "s) " + "########\n")
 
         if german_translation_model == "helsinki":
             input_ids_helsinki = tokenizer_helsinki.encode(text_english, return_tensors="pt")
@@ -153,88 +154,19 @@ def transcribe_to_txt(chunk_path, whisper_model="large", german_translation_mode
     deu_out = [x.encode('utf-8') for x in text_german]
 
     # Create output txt-files
+    if to_txt:
+        with open(os.path.join(chunk_path, "translation_english.txt"), 'wb') as f:
+            for entry in eng_out:
+                f.write(entry)
+        f.close()
+        with open(os.path.join(chunk_path, "transcript_original.txt"), 'wb') as f:
+            for entry in text_original:
+                f.write(entry)
+        f.close()
+        with open(os.path.join(chunk_path, "translation_german.txt"), 'wb') as f:
+            for entry in deu_out:
+                f.write(entry)
+        f.close()
 
-    with open(os.path.join(chunk_path, "translation_english.txt"), 'wb') as f:
-        for entry in eng_out:
-            f.write(entry)
-    f.close()
-    with open(os.path.join(chunk_path, "transcript_original.txt"), 'wb') as f:
-        for entry in text_original:
-            f.write(entry)
-    f.close()
-    with open(os.path.join(chunk_path, "translation_deutsch.txt"), 'wb') as f:
-        for entry in deu_out:
-            f.write(entry)
-    f.close()
+    return text_original, eng_out, deu_out
 
-
-def transcribe(chunk_path, whisper_model="large", german_translation_model="helsinki"):
-    """
-        :param chunk_path: path to the audio chunks. Chunks cannot be longer than 30 seconds
-        :param whisper_model: Size of the whisper model you want to use.
-                Available sizes are: tiny, base, small, medium, and large.
-                For references, visit: https://github.com/openai/whisper
-        :param german_translation_model: model for the translation english-german
-                Available options: helsinki, nllb
-    """
-
-    # german-english-model
-
-    if german_translation_model == "helsinki":
-        tokenizer_helsinki = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-de")
-        model_helsinki = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-de")
-    elif german_translation_model == "nllb":
-        en2de = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.en-de', tokenizer='moses', bpe='subword_nmt')
-        en2de.eval()
-        en2de.cuda()
-
-    # Whisper
-
-    whisper_model = whisper_model
-
-    filename = chunk_path.rsplit("\\", 1)[1]
-    print("File: " + filename)
-
-    text_original = []
-    text_english = []
-    text_german = []
-
-    model = whisper.load_model(whisper_model)
-
-    time_start = 0
-    time_end = 30
-
-    for file in os.listdir(chunk_path):
-        print("Working on part: " + file)
-
-        # Transcription + English translation
-
-        original_transcript = model.transcribe(os.path.join(chunk_path, file), task="transcribe")["text"].encode(
-            'utf-8')
-        text_english = model.transcribe(os.path.join(chunk_path, file), task="translate")["text"]
-
-        text_original.append(original_transcript.strip())
-
-        text_english.append(text_english.strip())
-
-        # Helsinki
-
-        if german_translation_model == "helsinki":
-            input_ids_helsinki = tokenizer_helsinki.encode(text_english, return_tensors="pt")
-            outputs_helsinki = model_helsinki.generate(input_ids_helsinki)
-            decoded_helsinki = tokenizer_helsinki.decode(outputs_helsinki[0], skip_special_tokens=True)
-
-            text_german.append(decoded_helsinki)
-
-        elif german_translation_model == "nllb":
-            text_german.append(en2de.translate(text_english))
-
-        time_start = time_end - 2
-        time_end = time_start + 30
-
-    # Encode to UTF-8 to avoid encoding errors caused by cyrillic characters
-
-    eng_out = [x.encode('utf-8') for x in text_english]
-    ger_out = [x.encode('utf-8') for x in text_german]
-
-    return text_original, eng_out, ger_out
